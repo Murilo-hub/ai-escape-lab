87ee7281006368eba3c62ac242e797cba4e11191
MuriloGB <105112352+Murilo-hub@users.noreply.github.com>
2026-05-13 09:42:05 -0300

    Delete lib directory

--- lib/game/formatDate.ts
+++
@@ -1,9 +0,0 @@
-export function formatDate(dateString: string) {
-  return new Date(dateString).toLocaleDateString('pt-BR', {
-    day: '2-digit',
-    month: '2-digit',
-    year: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-  })
-}
